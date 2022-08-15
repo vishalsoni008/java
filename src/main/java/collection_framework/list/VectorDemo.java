b05f9e6ca67c6e44e0fbfa27877497c24da83ea1
vishalsoni008 <vishalmundawra@gmail.com>
2022-08-15 12:56:58 +0530

    added Vector class example

---
+++ src/main/java/collection_framework/list/VectorDemo.java
@@ -0,0 +1,20 @@
+package collection_framework.list;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new Vector<>();
+
+        for (int i=0;i<5;i++){
+            list.add(i);
+        }
+
+        for (int number : list){
+            System.out.println(number);
+        }
+
+        System.out.println(list.isEmpty());
+    }
+}
