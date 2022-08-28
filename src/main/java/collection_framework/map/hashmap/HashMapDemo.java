34e9790a6721cfa572d09c579a669b71f026f0dd
vishalsoni008 <vishalmundawra@gmail.com>
2022-08-28 17:27:51 +0530

    added hash map demo

---
+++ src/main/java/collection_framework/map/hashmap/HashMapDemo.java
@@ -0,0 +1,24 @@
+package collection_framework.map.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1,"vishal");
+        map.put(2,"satya");
+        map.put(3,"saket");
+
+        System.out.println(map);
+
+        //itrating a map
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()){
+            System.out.println(entry);
+        }
+
+    }
+}
