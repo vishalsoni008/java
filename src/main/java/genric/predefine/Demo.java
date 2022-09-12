bc8eae2d6d6b527f228eb366a461e1b282f89bbf
vishalsoni008 <vishalmundawra@gmail.com>
2022-09-12 16:07:46 +0530

    added some genric and custom genric files

---
+++ src/main/java/genric/predefine/Demo.java
@@ -0,0 +1,14 @@
+package genric.predefine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        System.out.println(list);
+    }
+}
