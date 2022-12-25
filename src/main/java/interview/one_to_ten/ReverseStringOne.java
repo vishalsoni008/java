6adb7556609ef2f1a6e1f94a8786dd122144776e
vishalsoni008 <sonivishal.official@gmail.com>
2022-12-25 20:13:08 +0530

    added file in interview question

---
+++ src/main/java/interview/one_to_ten/ReverseStringOne.java
@@ -0,0 +1,14 @@
+package interview.one_to_ten;
+
+public class ReverseStringOne {
+    public static void main(String[] args) {
+        String name = "vishal";
+
+        String reverse = "";
+
+        for (int i=name.length()-1;i>=0;i--){
+            reverse = reverse+name.charAt(i);
+        }
+        System.out.println(reverse);
+    }
+}
